63f5ae1478a16ab8c8633f8824db0c7b4c75ea3e
Daniell w <126434546+Gregox0@users.noreply.github.com>
2024-03-27 17:35:58 -0300

    Delete projeto imoveis directory

--- projeto imoveis/scripts/loginCad.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const action = urlParams.get('action');
-
-    if (action === 'login') {
-    } else if (action === 'cadastro') {
-    }
-});
--- projeto imoveis/scripts/scripts.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('logButton').addEventListener('click', () => {
-    window.location.href = '../pages/loginCad.html?action=login'
-})
-document.getElementById('cadButton').addEventListener('click', () => {
-    window.location.href = '../pages/loginCad.html?action=cadastro'
-})
