22c45a850f309fb0953213c2d261710e1f8bf9e2
Daniell w <126434546+Gregox0@users.noreply.github.com>
2024-04-15 15:41:43 -0300

    Update loginCad.js

--- scripts/loginCad.js
+++ scripts/loginCad.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const action = urlParams.get('action');
 
-    if (action === 'login') {
-    } else if (action === 'cadastro') {
+    if (action === 'cadastro') {
+      document.querySelector('#title h3').innerHTML = 'Cadastro'
+      document.querySelector('#submit button').textContent = 'Cadastrar'
+      document.querySelector('#es').innerHTML = 'Ainda não tem uma conta? <span>Clique aqui.</span>'
     }
 });
