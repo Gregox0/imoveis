95f63e13bc5a750ba77226723f1ee7e177a2b00b
Daniell w. <126434546+Gregox0@users.noreply.github.com>
2024-08-14 16:53:32 -0300

    Update anunciar.js

--- scripts/anunciar.js
+++ scripts/anunciar.js
@@ -52,6 +52,9 @@ function toggleType(identify) {
 function changeDisplay(i) {
   document.getElementById(i + "Container").classList.remove('dpNo')
 }
+function changeDisplaydois(i) {
+    document.getElementById(i + "Container").classList.add('dpNo')
+}
 let opcoes = false
 function mostrarOpcoes() {
   if (!opcoes) {
@@ -102,24 +105,105 @@ function getSelectedValue(checkboxValue) {
   changeDisplay('cadastrar')
 }
 function verify() {
-  let cidade = document.getElementById('cidade')
-  let estado = document.getElementById('estado')
-  let rua = document.getElementById('rua')
-  let numero = document.getElementById('numero')
-  let complemento = document.getElementById('complemento')
+    let cidade = document.getElementById('cidade');
+    let estado = document.getElementById('estado');
+    let rua = document.getElementById('rua');
+    let numero = document.getElementById('numero');
+    let complemento = document.getElementById('complemento');
   
-  let endereco = {
-    cidade: cidade,
-    estado: estado,
-    rua: rua,
-    numero: numero,
-    complemento: complemento
-  }
-  for (let i in endereco) {
-    if (i != 'complemento') {
-      if (endereco[i].value == '') {
-        
+    let endereco = {
+      cidade: cidade.value,
+      estado: estado.value,
+      rua: rua.value,
+      numero: numero.value,
+      complemento: complemento.value
+    };
+
+    if (cidade.value == "") {
+        let container = document.getElementById('cidade');
+        setTimeout(() => {
+            container.style.borderColor = '#007ea7';
+          }, 1000);
+          container.style.borderColor = 'red';
+          return;
+   }
+   if (estado.value == "") {
+        let container = document.getElementById('estado');
+        setTimeout(() => {
+            container.style.borderColor = '#007ea7';
+        }, 1000);
+        container.style.borderColor = 'red';
+        return;
+   }
+   if (rua.value == "") {
+    let container = document.getElementById('rua');
+    setTimeout(() => {
+        container.style.borderColor = '#007ea7';
+    }, 1000);
+    container.style.borderColor = 'red';
+    return;
+    }
+    if (numero.value == "") {
+        let container = document.getElementById('numero');
+        setTimeout(() => {
+            container.style.borderColor = '#007ea7';
+        }, 1000);
+        container.style.borderColor = 'red';
+        return;
+   }
+   changeDisplaydois('want')
+   changeDisplaydois('type')
+   changeDisplaydois('slc')
+   changeDisplaydois('options')
+   changeDisplaydois('cad')
+   changeDisplaydois('cadastrar')
+   changeDisplay('sla')
+}
+let br = 0
+let currentbr = null
+function bathroomCount(value){
+    let brButton = document.getElementById('br' + value)
+    if (br == value) {
+      brButton.classList.remove('infoContainerbuttonclicked')
+      brButton.classList.add('infoContainerbutton')
+      br = 0
+      currentbr = null
+    }else{
+      br = value
+      if (currentbr != null) {
+        currentbr.classList.remove('infoContainerbuttonclicked')
+        currentbr.classList.add('infoContainerbutton')
       }
+      brButton.classList.remove('infoContainerbutton')
+      brButton.classList.add('infoContainerbuttonclicked')
+      currentbr = brButton
+    }
+    if(rm != 0 && br != 0){
+        changeDisplay('preco')
+        changeDisplay('size')
     }
   }
-        }
+  let rm = 0
+  let currentRoom = null
+  function roomsCount(value) {
+    let rmButton = document.getElementById('r' + value)
+    if (rm == value) {
+      rmButton.classList.remove('infoContainerbuttonclicked')
+      rmButton.classList.add('infoContainerbutton')
+      rm = 0
+      currentRoom = null
+    }else{
+      rm = value
+      if (currentRoom != null) {
+        currentRoom.classList.remove('infoContainerbuttonclicked')
+        currentRoom.classList.add('infoContainerbutton')
+      }
+      rmButton.classList.remove('infoContainerbutton')
+      rmButton.classList.add('infoContainerbuttonclicked')
+      currentRoom = rmButton
+    }
+    if(rm != 0 && br != 0){
+        changeDisplay('preco')
+        changeDisplay('size')
+    }
+  }
